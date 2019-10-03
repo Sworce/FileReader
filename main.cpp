d3e605517b1b80a8ed3ed6b4d1b3ed48816ac826
Sworce <Sworce>
2019-10-02 17:00:27 -0700

    added loop for repeat user input until exit

--- main.cpp
+++ main.cpp
@@ -9,18 +9,18 @@ int main()
   string fileName; //to get the name of the file to open
   string line; //to store a single line of a text file
   fstream fileStream; //to open a file for reading
-
   int numLines = 0;
   int numChars = 0;
+  char choice;
 
-  cout<<"What file do you want to open? ";
-  getline(cin, fileName);
+  do {
+    cout<<"What file do you want to open? ";
+    getline(cin, fileName);
   
   //STEP 1: open the fileStream for input, using the fileName specified
-  fileStream.open(fileName.c_str(), ios::in);
+    fileStream.open(fileName.c_str(), ios::in);
 
-  if(fileStream.is_open() /*STEP 2: check to see if the fileStream successfully opened*/ )
-  {
+    if(fileStream.is_open() /*STEP 2: check to see if the fileStream successfully opened*/ ) {
     cout<<fileName<<" opened.\nFILE CONTENTS:\n";
  
     while(!(fileStream.eof())) {
@@ -31,23 +31,36 @@ int main()
     cout << line << endl;
     numLines++;
     numChars = line.length();
-   }
-      
+     }
     }
-    cout << "File: " << fileName << endl;
-    cout << "Lines: " << numLines << endl;
-    cout << "Characters: " << numChars << endl;
     //STEP 3: repeat the following until the end-of-file (eof) has been reached...  
     // 3A: read a line from fileStream into the variable line
     // 3B: display the line, followed by an endline
-  
+    
+   }
+   
+   else {
+    cout << fileName << " could not be opend.\n";
+   }
+   
+    cout << "METADATA" << endl;
+    cout << "File: " << fileName << endl;
+    cout << "Lines: " << numLines << endl;
+    cout << "Characters: " << numChars << endl;
+   
+    cout << "Analyze another file (y/n)? ";
+    cin >> choice;
+    cin.ignore();
+   }while(choice == 'y' || choice == 'Y');
+    
     //STEP 4: close the fileStream
-    fileStream.close();
-  }
-  else
+   fileStream.close();
+  
+
+  /* else
   {
     cout<<fileName<<" could not be opened.\n";
-  }
+  }*/
 
   return 0;
 }
